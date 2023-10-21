6c24f4816f1d9f2242a79422b8d8b9e650607df1
EL HASSANI Ayoub <ayoblind@gmail.com>
2023-10-21 13:11:02 +0100

    env to the client config for github

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export default {
-    client_id: '91c8b71a150e11a90dfe',
-    client_secret: '41923d8dfda2f4e87b11864cf43748344f450d36',
+    client_id: process.env.client_id,
+    client_secret: process.env.client_secret,
     github_api_endpoint: 'https://api.github.com',
     app_name: 'Youcan Github Search',
 }
